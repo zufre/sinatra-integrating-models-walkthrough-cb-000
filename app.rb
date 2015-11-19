505b2f98b77789d0838da20d2018d43be4544289
vicfriedman <victoria.m.friedman@gmail.com>
2015-11-19 16:42:03 -0500

    solution

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    # binding.pry
     erb :results
-  end
+
 end
