6895e8674e26eac0c4c53f870fa3cf3938d3c54d
Brian Holland <bhollan@users.noreply.github.com>
2016-03-08 21:56:48 -0600

    re-order test specs to logical order
    The tests didn't make sense how they were.  TextAnalyzer shouldn't come last.

--- spec/integrating_models_sinatra_walkthrough_spec.rb
+++ spec/integrating_models_sinatra_walkthrough_spec.rb
@@ -13,20 +13,7 @@ describe App do
       expect(page).to have_field(:user_text)
     end
   end
-
-  describe 'POST /' do
-    it "displays string results" do 
-      visit '/'
-
-      fill_in(:user_text, :with => "Green Eggs and Ham")
-      click_button "submit"
-      expect(page).to have_text("Number of Words:4")
-      expect(page).to have_text("Vowels:5")
-      expect(page).to have_text("Consonants:10")
-      expect(page).to have_text("Most Common Letter: G, used 3 times")
-    end
-  end
-
+  
   describe 'TextAnalyzer Class' do 
       let!(:words) { TextAnalyzer.new("mirror mirror on the wall") }
 
@@ -38,6 +25,19 @@ describe App do
       expect(words.text).to eq("mirror mirror on the wall")
     end
 
+  end
+  
+  describe 'POST /' do
+    it "displays string results" do 
+      visit '/'
+
+      fill_in(:user_text, :with => "Green Eggs and Ham")
+      click_button "submit"
+      expect(page).to have_text("Number of Words:4")
+      expect(page).to have_text("Vowels:5")
+      expect(page).to have_text("Consonants:10")
+      expect(page).to have_text("Most Common Letter: G, used 3 times")
+    end
   end
 
 end
