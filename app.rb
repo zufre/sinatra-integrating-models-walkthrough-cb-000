cf618656d293d018b263dad399040b5fcfbce61f
Cedric David <cedric.david@btopenworld.com>
2018-09-19 07:09:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
+
 class App < Sinatra::Base
   get '/' do
     erb :index
@@ -8,6 +9,7 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(text_from_user)
 
     erb :results
   end
