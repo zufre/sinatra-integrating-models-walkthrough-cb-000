8f8aba53cda46a77717a88e2026d2dbd817c666a
Cedric David <cedric.david@btopenworld.com>
2018-09-19 07:18:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,8 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-    @analyzed_text = TextAnalyzer.new(text_from_user)
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
